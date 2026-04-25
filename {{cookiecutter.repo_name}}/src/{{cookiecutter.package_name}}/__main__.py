5083073929e96b993f1457613328c0c4960df356
paulafernalia <paula.fermin.cueto@gmail.com>
2026-04-25 21:01:16 +0200

    Clean code in main.py

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.package_name}}/__main__.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.package_name}}/__main__.py
@@ -1,33 +1,6 @@
-"""
-Main module for {{cookiecutter.project_name}}.
-
-This module provides an entry point for running the project
-as a script. It simply prints a greeting message to the console.
-"""
-
-from {{cookiecutter.package_name}}.optimization.engine import Engine
-from {{cookiecutter.package_name}}.utils.io import load_data_from_json, load_parameters_from_yaml
-
-# TODO
-
-print("Print something")
-
-
 def main() -> None:
-    """
-    Run package directly.
-
-    This function serves as the entry point when the package is run
-    directly.
-    """
-    data = load_data_from_json("data/instance.json")
-    params = load_parameters_from_yaml("conf/parameters.yaml")
-
-    solver = Engine(data)
-    solver.set_parameters(params)
-
-    solver.run()
-    solver.build_solution()
+    """Run package directly."""
+    raise NotImplementedError("This function is not implemented yet.")
 
 
 if __name__ == "__main__":
