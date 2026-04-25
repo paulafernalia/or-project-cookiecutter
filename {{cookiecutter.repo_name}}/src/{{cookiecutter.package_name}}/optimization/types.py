a2b2b51f5433da98947ba20aba3606bb3fb58dc9
paulafernalia <paula.fermin.cueto@gmail.com>
2026-04-25 19:24:17 +0200

    Remove source files

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.package_name}}/optimization/types.py
+++
@@ -1,7 +0,0 @@
-from typing import Dict, Tuple
-
-from highspy.highs import highs_var
-
-# Type alias for variable dictionaries keyed by index tuples
-Indices = Tuple[int, ...]
-VariableDict = Dict[Indices, highs_var]
